1af959266739634d8cdf15d2fbe6f65559275a28
kaustav Mani Deka <chwapnamkashyap@gmail.com>
2026-04-27 20:22:07 +0530

    Update ItemList.jsx

--- frontend/src/components/ItemList.jsx
+++ frontend/src/components/ItemList.jsx
@@ -6,7 +6,7 @@ function ItemList({ items, onEdit, onDelete }) {
             {items.map((item) => (
                 <li key={item._id}>
                     {item.name}
-                    <button onClick={() => onEdit(item)}>Edit</button>
+                    <button onClick={() => onEdit(item)}>Edit Name</button>
                     <button onClick={() => onDelete(item._id)}>Delete</button>
                 </li>
             ))}
